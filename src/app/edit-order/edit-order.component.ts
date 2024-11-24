d7e86f28c3c050b7ab1626d90e9b92302fcd4546
mohammadsadra <mohammadsadrahaeri@gmail.com>
2024-11-24 12:49:50 +0330

    Removed unnecessary files.

---
+++ src/app/edit-order/edit-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-order.component.html',
+  styleUrl: './edit-order.component.css'
+})
+export class EditOrderComponent {
+
+}
